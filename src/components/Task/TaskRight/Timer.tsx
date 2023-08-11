fa90d3078bc258449cf074c5556272ec1476bebc
kjw-blog <rkdwjddnr11@naver.com>
2023-08-11 23:26:55 +0900

    Feat ) 실시간 타이머 개선

--- src/components/Task/TaskRight/Timer.tsx
+++ src/components/Task/TaskRight/Timer.tsx
@@ -11,6 +11,7 @@ const defaultAmpm = defaultNow.format('a');
 const defaultTime = defaultNow.format('hh:mm');
 
 const Timer = () => {
+  const [timerMillisecond, setTimerMillisecond] = useState(1000);
   const [time, setTime] = useState<string>(`${defaultAmpm} ${defaultTime}`);
   const [date, setDate] = useState<string>(defaultNow.format('YYYY-MM-DD'));
 
@@ -24,12 +25,19 @@ const Timer = () => {
       const nowTime = now.format('hh:mm');
 
       setTime(`${ampm} ${nowTime}`);
-    }, 1000);
+
+      /**
+       * 렌더링시 최초 1회만 초단위로 시간 업데이트 후 1분에 1번 시간 업데이트
+       */
+      if (now.format('ss') === '00' && timerMillisecond === 1000) {
+        setTimerMillisecond(60000);
+      }
+    }, timerMillisecond);
 
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [timerMillisecond]);
 
   return (
     <TaskCard className={Style.wrapper}>
