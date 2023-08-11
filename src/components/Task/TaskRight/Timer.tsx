372b741a8a16374712b4ace9063c6e1821bf4615
kjw-blog <rkdwjddnr11@naver.com>
2023-08-11 23:19:28 +0900

    Feat ) 실시간 타이머 기능 추가

--- src/components/Task/TaskRight/Timer.tsx
+++ src/components/Task/TaskRight/Timer.tsx
@@ -1,14 +1,40 @@
 import Style from '@sass/Task/TaskRight/Timer.module.scss';
 import TaskCard from '../TaskCard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko');
+const defaultNow = dayjs();
+
+const defaultAmpm = defaultNow.format('a');
+const defaultTime = defaultNow.format('hh:mm');
 
 const Timer = () => {
-  const [time, setTime] = useState();
+  const [time, setTime] = useState<string>(`${defaultAmpm} ${defaultTime}`);
+  const [date, setDate] = useState<string>(defaultNow.format('YYYY-MM-DD'));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = dayjs();
+
+      setDate(now.format('YYYY-MM-DD'));
+
+      const ampm = now.format('a');
+      const nowTime = now.format('hh:mm');
+
+      setTime(`${ampm} ${nowTime}`);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <TaskCard className={Style.wrapper}>
-      <p>오후 10:24</p>
-      <p>2023-08-11</p>
+      <p>{time}</p>
+      <p>{date}</p>
     </TaskCard>
   );
 };
