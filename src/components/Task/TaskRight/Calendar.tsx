954ef6415d641644c7b23d7e74df584132e07cd3
kjw-blog <rkdwjddnr11@naver.com>
2023-08-22 22:53:49 +0900

    Feat ) 캘린더 버튼 추가

--- src/components/Task/TaskRight/Calendar.tsx
+++ src/components/Task/TaskRight/Calendar.tsx
@@ -1,12 +1,17 @@
+import Arrow from '@components/UI/Svg/Arrow';
 import Style from '@sass/Task/TaskRight/Calendar.module.scss';
+import dayjs from 'dayjs';
 import { useState, forwardRef, useImperativeHandle } from 'react';
 
 export type CalendarType = {
   close: () => void;
 };
 
+const defaultNow = dayjs();
+
 const Calendar = forwardRef<CalendarType>((props, ref) => {
   const [onClose, setOnClose] = useState(false);
+  const formatNow = defaultNow.format('M월 DD일 dd요일');
 
   const calendarCloseHandler = () => {
     setOnClose(true);
@@ -19,7 +24,10 @@ const Calendar = forwardRef<CalendarType>((props, ref) => {
   return (
     <div className={`${Style['calender-wrap']} ${onClose && Style.close}`}>
       <div className={Style.title}>
-        <span>8월 11일 금요일</span>
+        <span>{formatNow}</span>
+        <button>
+          <Arrow />
+        </button>
       </div>
     </div>
   );
---
+++ src/components/UI/Svg/Arrow.tsx
@@ -0,0 +1,20 @@
+const Arrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      style={{ width: '10px', height: '10px' }}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 15.75l7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+};
+
+export default Arrow;
